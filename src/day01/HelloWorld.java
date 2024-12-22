0e2297c6e565a8653aec3e9dcf79de9c1fc492c8
karina-erkanian <carryk.erk@gmail.com>
2024-12-21 23:01:09 -0500

    HelloCydeo Class

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -1,12 +1,15 @@
 package day01;
 
 public class HelloWorld {
-    //enable git -> main menu -> VSC ->enable control integration
-    //to ignore a file - right click on it -> git -> add to gitignore
-    //in order to save it in the local repository -> ad to the staging area first by giving add command
+//enable git -> main menu -> VSC ->enable control integration
+//to ignore a file - right click on it -> git -> add to gitignore
+//in order to save it in the local repository -> ad to the staging area first by giving add command
 //to save in the local repository -> give commit command(include message -required)
+//if it's highlighted red that means it's in the working directory
+//green means it's in the staging area(it was added) but has not been saved in the local repository yet
+//if it's white color it means it was saved in the local repository
+    //blue means some changes were made in the working directory
 
 
-
-
+    //to add it to the remote repository -> main menu ->git(on the top instead of VCS)
 }
