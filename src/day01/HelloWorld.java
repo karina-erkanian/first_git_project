c51dcddbef515485df8057fa405222261e1222d3
karina-erkanian <carryk.erk@gmail.com>
2024-12-21 22:38:34 -0500

    HelloWorld Class

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class HelloWorld {
+    //enable git -> main menu -> VSC ->enable control integration
+    //to ignore a file - right click on it -> git -> add to gitignore
+    //in order to save it in the local repository -> ad to the staging area first by giving add command
+//to save in the local repository -> give commit command(include message -required)
+
+
+
+
+}
